d3d055b443e8461f02354b5035ccb325ed570aba
Thanh Truong <thanhgvsu@gmail.com>
2019-01-15 09:08:37 -0500

    Implemented code to launch QuizCardBuilder screeen

---
+++ src/e/flashcard/MainApp.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e.flashcard;
+
+import e.flashcard.view_controller.BuilderController;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author thanhtruong
+ */
+public class MainApp extends Application {
+    
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Quiz Card Builder");
+        
+        showCardBuilderScreen();
+    }
+    
+    public void showCardBuilderScreen(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view_controller/Builder.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+            // Give controller access to the main app
+            BuilderController controller = loader.getController();
+            controller.setMainApp(this);
+            controller.setDialogStage(primaryStage);
+            
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
--- src/e/flashcard/view_controller/BuilderController.java
+++ src/e/flashcard/view_controller/BuilderController.java
@@ -5,9 +5,16 @@
  */
 package e.flashcard.view_controller;
 
+import e.flashcard.MainApp;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +22,52 @@ import javafx.fxml.Initializable;
  * @author thanhtruong
  */
 public class BuilderController implements Initializable {
+    
+    @FXML
+    private MenuItem saveMenu;
+
+    @FXML
+    private MenuItem newMenu;
+
+    @FXML
+    private TextArea questionTextArea;
+
+    @FXML
+    private TextArea answerTextArea;
+
+    @FXML
+    private Button nextCardButton;
+
+    @FXML
+    void handleNewCard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNextCard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSaveCard(ActionEvent event) {
+
+    }
+    
+    private MainApp mainApp;
+    private Stage dialogStage;
+        
+    
+    public void setMainApp(MainApp mainApp){
+        this.mainApp = mainApp;
+    }
+    
+    /**
+     * Sets the stage of the "alert.initOwner(dialogStage)". Called by MainApp
+     * @param dialogStage 
+     */
+    public void setDialogStage(Stage dialogStage){
+        this.dialogStage = dialogStage;
+    }
 
     /**
      * Initializes the controller class.
